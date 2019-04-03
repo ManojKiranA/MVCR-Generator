902c7e116656f1a041f34529438ee621d1841a17
Manojkiran Appathurai <manojkiran.a@Postiefs.com>
2019-04-03 15:25:12 +0530

    added basecommand to check

---
+++ src/Commands/BaseCommand.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Manojkiran\MVCRGenerator;
+
+use Illuminate\Console\Command;
+
+class BaseMVCRGenerator extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'mvcr::generate {controlName : Class (singular) for example Post}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Generated the Model Views and Controller for the laravel Application';
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        echo ' the initail stage has been passes';
+    }
+}
